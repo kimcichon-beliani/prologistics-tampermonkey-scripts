c0355921d44ace434bc74d3614902de9e8d16955
kimcichon-beliani <klaudia.cichon@beliani.fr>
2026-07-22 13:13:34 +0200

    Update empty-rows-highlight.user.js

--- empty-rows-highlight.user.js
+++ empty-rows-highlight.user.js
@@ -8,6 +8,9 @@
 // @match        https://www.prologistics.info/total_cycle_time.php*
 // @run-at       document-idle
 // @grant        none
+// @updateURL https://raw.githubusercontent.com/kimcichon-beliani/prologistics-tampermonkey-scripts/main/empty-rows-highlight.user.js
+// @downloadURL https://raw.githubusercontent.com/kimcichon-beliani/prologistics-tampermonkey-scripts/main/empty-rows-highlight.user.js
+
 // ==/UserScript==
 
 // Autor: kimrioter
